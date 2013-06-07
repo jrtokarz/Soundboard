3bc292fbe5b652222e6d35eb9f1b65a0e4cf3f15
jrt <jrt@plutonium.binarycows.com>
2013-06-07 15:13:47 +0200

    Playing about creating subclassed QPushButtons

--- soundboard.py
+++ soundboard.py
@@ -7,6 +7,13 @@
 import sys
 from PyQt4 import QtGui
 
+class SBButton(QtGui.QPushButton):
+    def __init__(self, name, parent):
+        super(SBButton, self).__init__(name, parent)
+        print "Width: " + str(parent.width()) + " height: " + str(parent.height())
+        
+    def resizeEvent(self, resize_event):
+        print "SBButton: I got resized"
 
 class Example(QtGui.QWidget):
     
@@ -20,20 +27,23 @@ class Example(QtGui.QWidget):
         QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
         
         self.setToolTip('This is a <b>QWidget</b> widget')
-        
-        btn = QtGui.QPushButton('Button', self)
-        btn.setToolTip('This is a <b>QPushButton</b> widget')
-        btn.resize(btn.sizeHint())
-        btn.move(50, 50)       
-        
-        self.setGeometry(300, 300, 250, 150)
+        self.setGeometry(300, 300, 500, 300)
         self.setWindowTitle('Tooltips')    
-        self.show()
+        
+        self.btn = SBButton('Button', self)
+        self.btn.setToolTip('This is a <b>QPushButton</b> widget')
+        
+    def resizeEvent(self, resize_event):
+        super(Example, self).resizeEvent(resize_event)
+        print "I got resized"
+        self.btn.resize(self.width()/5, self.height()/5)
+        
         
 def main():
     
     app = QtGui.QApplication(sys.argv)
     ex = Example()
+    ex.show()
     sys.exit(app.exec_())
 
 
